de6c5e90527c610cc09f950a4b40643df675e200
Yrtx <andrey.solovynov@yandex.ru>
2022-11-15 23:57:02 +0300

    test_4

--- app.js
+++ app.js
@@ -22,4 +22,8 @@ mongoose.connect('mongodb://localhost/mestodb');
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Страница не найдена' });
+});
+
 app.listen(PORT);
